12b28e4de3565d22ae59580760ae3b8436ca615a
MarainBadea <badea.alexandrumarian@gmail.com>
2021-07-22 16:46:28 +0300

    [65] Make tests prettier

--- src/components/FreakTile/__test__/FreakTile.test.jsx
+++ src/components/FreakTile/__test__/FreakTile.test.jsx
@@ -4,26 +4,41 @@ import FreakTile from '../FreakTile';
 
 describe('FreakTile', () => {
   it('should render the freak tile', () => {
-    render(<FreakTile id={ 5 } picture="some-url" name="name" />);
+    const id = 1;
+    const picture = 'some-url';
+    const name = 'Anda';
+
+    render(<FreakTile id={ id } picture={ picture } name={ name } />);
 
     expect(screen.getByTestId('freak-tile')).toBeInTheDocument();
   });
 
   it('should render the tile as a hyperlink', () => {
-    const id = 6;
-    render(<FreakTile id={ id } picture="some-url" name="name" />);
+    const id = 1;
+    const picture = 'some-url';
+    const name = 'Anda';
+
+    render(<FreakTile id={ id } picture={ picture } name={ name } />);
 
     expect(screen.getByTestId('freak-tile')).toHaveAttribute('href', `/freaks/${ id }`);
   });
 
   it('should render a name on the tile', () => {
-    render(<FreakTile id={ 5 } picture="some-url" name="name" />);
+    const id = 1;
+    const picture = 'some-url';
+    const name = 'Anda';
 
-    expect(screen.getByTestId('freak-name')).toHaveTextContent('name');
+    render(<FreakTile id={ id } picture={ picture } name={ name } />);
+
+    expect(screen.getByTestId('freak-name')).toHaveTextContent('Anda');
   });
 
   it('should render a picture for the tile', () => {
-    render(<FreakTile id={ 5 } picture="some-url" name="name" />);
+    const id = 1;
+    const picture = 'some-url';
+    const name = 'Anda';
+
+    render(<FreakTile id={ id } picture={ picture } name={ name } />);
 
     expect(screen.getByTestId('freak-picture')).toHaveAttribute('src', 'some-url');
   });
