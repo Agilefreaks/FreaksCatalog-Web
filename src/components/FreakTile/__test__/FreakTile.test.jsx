7fc77a0f6a0d55a26b77a8b3fdd27495d2017df7
MarainBadea <badea.alexandrumarian@gmail.com>
2021-07-23 09:40:44 +0300

    [65] Remove the duplication

--- src/components/FreakTile/__test__/FreakTile.test.jsx
+++ src/components/FreakTile/__test__/FreakTile.test.jsx
@@ -3,41 +3,29 @@ import { render, screen } from '@testing-library/react';
 import FreakTile from '../FreakTile';
 
 describe('FreakTile', () => {
-  it('should render the freak tile', () => {
-    const id = 1;
-    const picture = 'some-url';
-    const name = 'Anda';
+  const id = 1;
+  const picture = 'some-url';
+  const name = 'Anda';
 
+  it('should render the freak tile', () => {
     render(<FreakTile id={ id } picture={ picture } name={ name } />);
 
     expect(screen.getByTestId('freak-tile')).toBeInTheDocument();
   });
 
   it('should render the tile as a hyperlink', () => {
-    const id = 1;
-    const picture = 'some-url';
-    const name = 'Anda';
-
     render(<FreakTile id={ id } picture={ picture } name={ name } />);
 
     expect(screen.getByTestId('freak-tile')).toHaveAttribute('href', `/freaks/${ id }`);
   });
 
   it('should render a name on the tile', () => {
-    const id = 1;
-    const picture = 'some-url';
-    const name = 'Anda';
-
     render(<FreakTile id={ id } picture={ picture } name={ name } />);
 
     expect(screen.getByTestId('freak-name')).toHaveTextContent(name);
   });
 
   it('should render a picture for the tile', () => {
-    const id = 1;
-    const picture = 'some-url';
-    const name = 'Anda';
-
     render(<FreakTile id={ id } picture={ picture } name={ name } />);
 
     expect(screen.getByTestId('freak-picture')).toHaveAttribute('src', picture);
