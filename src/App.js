7e7f31f8697ef272d33cd936d1fb1bc9fe080552
MarianBadea <85878723+MarianBadea@users.noreply.github.com>
2021-08-03 10:23:13 +0300

    [77] Create modal component (#5)
    * [77] Create modal component
    
    * [77] Create modal structure & show/hide modal
    
    * [77] Add styling
    
    * [77] Add styling to title &  buttons & overlay
    
    * [77] Rename and reorganize the modal component & delete Reset and Apply buttons & change header style
    
    * [77] Add changes
    
    * [77] Write tests for the modal component & add some changes
    
    * [77] Resolve comments and mock icon library
    
    * [77]  Change to scss files
    
    * [77]  Change to scss files
    
    * [77] Import App.scss in App.js
    
    * [77] Change in imports to scss
    
    * [77] Resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import './App.scss';
 import FreakTile from './components/FreakTile/FreakTile';
 import { freaks } from './mock-data/freaks.json';
+import Modal from './components/Modal/Modal';
+
+library.add(faTimes);
 
 function App() {
+  const [ isOpen, setIsOpen ] = useState(false);
   const result = freaks.map((user) => (
     <FreakTile
       id={ user.id }
@@ -14,7 +20,19 @@ function App() {
   ));
 
   return (
-    <div className="content">{ result }</div>
+    <div>
+      <button className="app__button" type="button" onClick={ () => setIsOpen(true) }>Skills</button>
+      <Modal
+        title="Skills"
+        onClose={ () => setIsOpen(false) }
+        isOpen={ isOpen }
+        headerContent={ null }
+        footerContent={ null }
+      >
+        <p>Modal</p>
+      </Modal>
+      <div className="content">{ result }</div>
+    </div>
   );
 }
 
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Modal.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function getHeader(onClose, title, headerContent) {
+  return (
+    <div className="modal__header">
+      <div className="modal__header-left-corner">
+        <button className="modal__close-button" type="button" onClick={ onClose }>
+          <FontAwesomeIcon icon="times" />
+        </button>
+      </div>
+      <div className="modal__header-center">
+        <h1 className="modal__title" data-testid="modal-title">{ title }</h1>
+      </div>
+      <div className="modal__header-right-corner" data-testid="modal-header-right-corner">{ headerContent }</div>
+    </div>
+  );
+}
+
+function getBody(bodyContent) {
+  return (
+    <div className="modal__body" data-testid="modal-body">
+      { bodyContent }
+    </div>
+  );
+}
+
+function getFooter(footerContent) {
+  return (
+    !!footerContent && (
+      <div className="modal__footer" data-testid="modal-footer">
+        { footerContent }
+      </div>
+    )
+  );
+}
+
+function Modal({
+  isOpen,
+  onClose,
+  title,
+  headerContent,
+  children,
+  footerContent,
+}) {
+  return (
+    <div className={ `modal-wrapper ${ isOpen ? '' : '--hidden' }` } data-testid="modal-wrapper">
+      <div className="modal-overlay" />
+      <div className="modal">
+        <div className="modal__content">
+          { getHeader(onClose, title, headerContent) }
+          { getBody(children) }
+          { getFooter(footerContent) }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  title: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  headerContent: PropTypes.node,
+  children: PropTypes.node.isRequired,
+  footerContent: PropTypes.node,
+};
+
+Modal.defaultProps = {
+  headerContent: null,
+  footerContent: null,
+};
+
+export default Modal;
