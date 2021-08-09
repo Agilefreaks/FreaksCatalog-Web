2a60189f1134be1218886a2d818ed6bad4c45e67
MarainBadea <badea.alexandrumarian@gmail.com>
2021-08-09 15:37:20 +0300

    [139] Store the information changed in the forms

--- src/components/AddFreakModal/AddFreakModal.jsx
+++ src/components/AddFreakModal/AddFreakModal.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import Modal from '../Modal/Modal';
 import EditFreakForm from '../EditFreakForm/EditFreakForm';
 
 function AddFreakModal({ title, isOpen, onClose }) {
-  const [ checkedState, setCheckedState ] = useState([]);
-
   return (
     <Modal
       title={ title }
       isOpen={ isOpen }
-      checkedState={ checkedState }
-      headerContent={ <button className="filter__button-reset" type="button" onClick={ () => setCheckedState([]) }>Reset</button> }
+      headerContent={ <button className="filter__button-reset" type="button">Reset</button> }
       footerContent={ <Button className="filter__button-apply" variant="primary py-2 px-5 " disabled>Summit</Button> }
       onClose={ onClose }
     >
--- src/components/EditFreakForm/EditFreakForm.jsx
+++ src/components/EditFreakForm/EditFreakForm.jsx
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 
+const FreakModelKeys = {
+  firstName: 'firstName',
+  lastName: 'lastname',
+  email: 'email',
+  description: 'description',
+  role: 'role',
+  level: 'level',
+  norm: 'norm',
+  skills: 'skills',
+};
+
 function EditFreakForm() {
+  const [ values, setValues ] = useState({
+    [FreakModelKeys.firstName]: '',
+    [FreakModelKeys.lastName]: '',
+    [FreakModelKeys.email]: '',
+    [FreakModelKeys.description]: '',
+    [FreakModelKeys.role]: '',
+    [FreakModelKeys.level]: '',
+    [FreakModelKeys.norm]: '',
+    [FreakModelKeys.skills]: '',
+  });
+
+  const set = (name) => ({ target: { value } }) => {
+    setValues((oldValues) => ({ ...oldValues, [name]: value }));
+  };
+
   return (
     <Form className=" mx-2 p-3">
       <Form.Group className="mb-3">
         <Form.Label>Your name</Form.Label>
         <Row>
           <Col>
-            <Form.Control placeholder="First name" />
+            <Form.Control placeholder="First name" value={ values.firstName } onChange={ set(FreakModelKeys.firstName) } />
           </Col>
           <Col>
-            <Form.Control placeholder="Last name" />
+            <Form.Control placeholder="Last name" value={ values.lastName } onChange={ set(FreakModelKeys.lastName) } />
           </Col>
         </Row>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" value={ values.email } onChange={ set(FreakModelKeys.email) } />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+      <Form.Group className="mb-3">
         <Form.Label>Description</Form.Label>
-        <Form.Control as="textarea" rows={ 3 } />
+        <Form.Control as="textarea" rows={ 3 } value={ values.description } onChange={ set(FreakModelKeys.description) } />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>Role</Form.Label>
-        <Form.Select>
+        <Form.Select value={ values.role } onChange={ set(FreakModelKeys.role) }>
           <option>Founder</option>
           <option>Frontend Dev</option>
           <option>Backend Dev</option>
@@ -37,7 +63,7 @@ function EditFreakForm() {
 
       <Form.Group className="mb-3">
         <Form.Label>Level</Form.Label>
-        <Form.Select>
+        <Form.Select value={ values.level } onChange={ set(FreakModelKeys.level) }>
           <option>Master</option>
           <option>Expert</option>
           <option>Proficient</option>
@@ -49,19 +75,19 @@ function EditFreakForm() {
 
       <Form.Group className="mb-3">
         <Form.Label>Norm</Form.Label>
-        <Form.Select>
+        <Form.Select value={ values.norm } onChange={ set(FreakModelKeys.norm) }>
           <option>Full time</option>
           <option>Part time</option>
         </Form.Select>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+      <Form.Group className="mb-3">
         <Form.Label>Skills</Form.Label>
         <Form.Text className="text-muted">
           <br />
           Example (Elm, JS, Swift...)
         </Form.Text>
-        <Form.Control as="textarea" rows={ 3 } />
+        <Form.Control as="textarea" rows={ 3 } value={ values.skills } onChange={ set(FreakModelKeys.skills) } />
       </Form.Group>
     </Form>
   );
