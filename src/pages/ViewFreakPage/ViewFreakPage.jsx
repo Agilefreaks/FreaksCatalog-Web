354808fc2e773ad02df36a053688e840b03b46fe
MarainBadea <badea.alexandrumarian@gmail.com>
2021-08-26 11:20:33 +0300

    [198] Add '?edit' to /freaks/:id

--- src/pages/ViewFreakPage/ViewFreakPage.jsx
+++ src/pages/ViewFreakPage/ViewFreakPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory, useLocation } from 'react-router-dom';
 import { freaks } from '../../mock-data/freaks.json';
 import FreakDetails from '../../components/FreakDetails/FreakDetails';
 import './ViewFreakPage.scss';
@@ -10,15 +10,34 @@ function findFreak(id) {
   return (freak) => freak.id === id;
 }
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 const modals = {
   EDIT: 'editFreak',
+  NONE: '',
 };
 
 function ViewFreakPage() {
+  const query = useQuery();
+  const editQueryParam = query.get('edit');
+
+  const [ openModal, setOpenModal ] = useState(modals.EDIT);
+
   const { id } = useParams();
   const parsedId = parseInt(id, 10);
 
-  const [ openModal, setOpenModal ] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const editModalIsOpen = openModal === modals.EDIT;
+    if (editModalIsOpen) {
+      history.push(`/freaks/${ id }?edit`);
+    } else {
+      history.push(`/freaks/${ id }`);
+    }
+  }, [ openModal ]);
 
   const freak = freaks.find(findFreak(parsedId));
 
@@ -38,15 +57,15 @@ function ViewFreakPage() {
           className="btn btn-outline-secondary app-button -medium "
           variant="default"
           data-testid="view-freak-edit-button"
-          onClick={ () => setOpenModal(modals.ADD) }
+          onClick={ () => setOpenModal(modals.EDIT) }
         >
           Edit
         </Button>
         <EditFreakModal
           freak={ freak }
           title="Edit Freak"
-          isOpen={ openModal === modals.ADD }
-          onClose={ () => setOpenModal(null) }
+          isOpen={ openModal === modals.EDIT }
+          onClose={ () => setOpenModal(modals.NONE) }
         />
       </div>
     </div>
