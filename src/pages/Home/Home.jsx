12ed198b4c6be6a6f37235cb6d8038b4886db9ec
MarainBadea <badea.alexandrumarian@gmail.com>
2021-09-04 18:34:17 +0300

    [212] Create home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import homeImg from '../../images/homeIMG.jpeg';
+import './Home.scss';
 
 function Home() {
   return (
-    <p>
-      Agile Freaks
-      <br />
-      Who we are and what makes us freaks?
-    </p>
+    <div className="home">
+      <h1>Who are we?</h1>
+      <p>
+        The short version, we are a company founded in 2014, by developers for developers,
+        with 26 freaks and still counting.
+        <hr />
+        The long version is we are a flat structure company,
+        with no corporate ladders and micromanagement.
+        Transparency is key for the way we work and everyone has access
+        to all information regarding the company. And by this, we also mean
+        financial data, like income, expenses and how we calculate salaries too.
+      </p>
+      <img className="home__img" src={ homeImg } alt="home" />
+    </div>
   );
 }
 
