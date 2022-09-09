849d4d6b612c885ccf31c3e5e2e09de99667e08a
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-09 16:42:50 +0300

    Fix console warnings

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <div className="home">
       <div className="home__content">
         <h1 className="home__title">Who are we?</h1>
-        <p className="home__description">
+        <span className="home__description">
           The short version, we are a company founded in 2014, by developers for
           developers, with 26 freaks and still counting.
           <hr />
@@ -15,7 +15,7 @@ function Home() {
           and micromanagement. Transparency is key for the way we work and everyone has
           access to all information regarding the company. And by this, we also mean
           financial data, like income, expenses and how we calculate salaries too.
-        </p>
+        </span>
         <img className="home__img" src={ homeImg } alt="home" />
       </div>
     </div>
