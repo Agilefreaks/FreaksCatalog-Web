b85aaa37f7c6fab372346c7b46dafdd97c5a3daa
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-09 14:13:56 +0300

    Filter freaks by skill and project at the same time

--- src/components/FreaksGrid/FreaksGrid.jsx
+++ src/components/FreaksGrid/FreaksGrid.jsx
@@ -19,19 +19,43 @@ function FreaksGrid({ freaks }) {
       ))};
 
       const getFilteredFreaks = () => {
-        if (filters.skills.length === 0) {
-          return freaks;
-        }
+        const getSkillFilteredFreaks = () => {
+          if (filters.skills.length === 0) {
+            return freaks;
+          }
 
-        const isFilteredTech = tech => {
-          return filters.skills.some(filter => filter === tech.name)
+          const isFilteredTech = tech => {
+            return filters.skills.some(filter => filter === tech.name)
+          };
+
+          const hasFilteredTech = freak => {
+            return freak.technologies.some(tech => isFilteredTech(tech))
+          };
+
+          return freaks.filter( freak => hasFilteredTech(freak));
         };
 
-        const hasFilteredTech = freak => {
-          return freak.technologies.some(tech => isFilteredTech(tech))
+        const getProjectFilteredFreaks = () => {
+          if (filters.projects.length === 0) {
+            return freaks;
+          }
+
+          const isFilteredProj = proj => {
+            return filters.projects.some(filter => filter === proj.name)
+          };
+
+          const hasFilteredProj = freak => {
+            return freak.projects.some(tech => isFilteredProj(tech))
+          };
+
+          return freaks.filter( freak => hasFilteredProj(freak));
         };
 
-        return freaks.filter( freak => hasFilteredTech(freak));
+        const intersection = (arr1, arr2) => {
+          return arr1.filter(x => arr2.includes(x));
+        };
+
+        return intersection(getSkillFilteredFreaks(), getProjectFilteredFreaks());
       }
 
       return getFreakTiles(getFilteredFreaks(freaks));
