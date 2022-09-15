90af2dd4b385e63adfec0b38f98d6e184c54d2d5
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-15 10:54:55 +0300

    Convert title letters to spans

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function App() {
       <Router>
         <div className="app">
           <div className="app__title">
-            <div className="title-letter"><p>F</p></div>
-            <div className="title-letter"><p>R</p></div>
-            <div className="title-letter"><p>E</p></div>
-            <div className="title-letter"><p>A</p></div>
-            <div className="title-letter"><p>K</p></div>
-            <div className="title-letter"><p>S</p></div>
+            <span className="title-letter">F</span>
+            <span className="title-letter">R</span>
+            <span className="title-letter">E</span>
+            <span className="title-letter">A</span>
+            <span className="title-letter">K</span>
+            <span className="title-letter">S</span>
             <Menu />
           </div>
           <div className="app__content">
