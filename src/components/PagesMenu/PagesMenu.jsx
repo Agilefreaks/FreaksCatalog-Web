a355d6dc66ada73c5db64ee3f43994885e7adb23
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-16 11:59:51 +0300

    Replaced the content of PagesMenu with MenuItems

--- src/components/PagesMenu/PagesMenu.jsx
+++ src/components/PagesMenu/PagesMenu.jsx
@@ -4,28 +4,14 @@ import logo from '../../images/logo-only-blue.svg';
 import projectLogo from '../../images/logo-projects.png';
 import homeLogo from '../../images/home.png';
 import './PagesMenu.scss';
+import MenuItem from './MenuItem';
 
 function PagesMenu() {
   return (
     <div className="menu-container">
-      <Link to="/">
-        <button type="button" className="menu-container__item">
-          <img className="menu-container__item-img" src={ homeLogo } />
-          <p>Home</p>
-        </button>
-      </Link>
-      <Link to="/freaks">
-        <button type="button" className="menu-container__item">
-          <img className="menu-container__item-img" src={ logo } />
-          <p>Freaks</p>
-        </button>
-      </Link>
-      <Link to="/">
-        <button type="button" className="menu-container__item">
-          <img className="menu-container__item-img" src={ projectLogo } />
-          <p>Projects</p>
-        </button>
-      </Link>
+      <MenuItem path="/" label="Home" image={ homeLogo } />
+      <MenuItem path="/freaks" label="Freaks" image={ logo } />
+      <MenuItem path="/" label="Projects" image={ projectLogo } />
     </div>
   );
 }
