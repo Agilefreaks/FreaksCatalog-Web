2c41a4f2fba3bcd9c850bdf72076fce24dfa5c45
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-16 11:45:47 +0300

    Move PagesMenu css to separate file

--- src/components/PagesMenu/PagesMenu.jsx
+++ src/components/PagesMenu/PagesMenu.jsx
@@ -3,25 +3,26 @@ import { Link } from 'react-router-dom';
 import logo from '../../images/logo-only-blue.svg';
 import projectLogo from '../../images/logo-projects.png';
 import homeLogo from '../../images/home.png';
+import './PagesMenu.scss';
 
 function PagesMenu() {
   return (
-    <div className="app__nav">
+    <div className="menu-container">
       <Link to="/">
-        <button type="button" className="nav__button">
-          <img className="nav__button-img" src={ homeLogo } alt="Nav" />
+        <button type="button" className="menu-container__item">
+          <img className="menu-container__item-img" src={ homeLogo } />
           <p>Home</p>
         </button>
       </Link>
       <Link to="/freaks">
-        <button type="button" className="nav__button">
-          <img className="nav__button-img" src={ logo } alt="Nav" />
+        <button type="button" className="menu-container__item">
+          <img className="menu-container__item-img" src={ logo } />
           <p>Freaks</p>
         </button>
       </Link>
       <Link to="/">
-        <button type="button" className="nav__button">
-          <img className="nav__button-img" src={ projectLogo } alt="Nav" />
+        <button type="button" className="menu-container__item">
+          <img className="menu-container__item-img" src={ projectLogo } />
           <p>Projects</p>
         </button>
       </Link>
