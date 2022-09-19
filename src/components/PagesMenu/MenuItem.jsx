2cdfe09624aac0e6fbd7f13d1dda24976203eaf8
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-19 09:57:46 +0300

    Fix menuitem lint warnings

--- src/components/PagesMenu/MenuItem.jsx
+++ src/components/PagesMenu/MenuItem.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function MenuItem({ path, label, image, onClick }) {
   return (
     <Link to={ path }>
-      <button type="button" className="menu-container__item" onClick={ onClick } >
-        <img className="menu-container__item-img" src={ image } />
+      <button type="button" className="menu-container__item" onClick={ onClick }>
+        <img className="menu-container__item-img" src={ image } alt={ `${ label } icon` } />
         <p className="menu-container__item-label">{ label }</p>
       </button>
     </Link>
