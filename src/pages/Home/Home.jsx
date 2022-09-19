2bfd127be79db28cef324c0c8163c60abc4624c5
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-19 12:27:28 +0300

    Divide span in two paragraphs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,15 +7,17 @@ function Home() {
     <div className="home">
       <div className="home__content">
         <h1 className="home__title">Who are we?</h1>
-        <span className="home__description">
+        <p className="home__description">
           The short version, we are a company founded in 2014, by developers for
           developers, with 26 freaks and still counting.
-          <hr />
+        </p>
+        <hr />
+        <p className="home__description">
           The long version is we are a flat structure company, with no corporate ladders
           and micromanagement. Transparency is key for the way we work and everyone has
           access to all information regarding the company. And by this, we also mean
           financial data, like income, expenses and how we calculate salaries too.
-        </span>
+        </p>
         <img className="home__img" src={ homeImg } alt="home" />
       </div>
     </div>
