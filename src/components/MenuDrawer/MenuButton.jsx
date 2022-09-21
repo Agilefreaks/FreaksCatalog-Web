97eecc32708c6730290b8de587b5157bfa1e73b7
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-21 11:17:06 +0300

    Remove unused imports

--- src/components/MenuDrawer/MenuButton.jsx
+++ src/components/MenuDrawer/MenuButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from 'react-bootstrap';
 import './styles/menu-button.scss';
--- src/components/MenuDrawer/MenuDrawer.jsx
+++ src/components/MenuDrawer/MenuDrawer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Drawer from '../Drawer/Drawer';
 import PagesMenu from '../PagesMenu/PagesMenu';
