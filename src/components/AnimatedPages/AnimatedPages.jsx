6fb33f4d630a5658330ca89eea630233babdfa75
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-26 16:29:18 +0300

    Add proptypes for AnimatedPages functions

--- src/components/AnimatedPages/AnimatedPages.jsx
+++ src/components/AnimatedPages/AnimatedPages.jsx
@@ -1,42 +1,50 @@
 import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-function FadeTransitionAnimatedPage(props) {
+function FadeTransitionAnimatedPage({ children }) {
   return (
     <motion.div
-      className={ props.className }
       initial={ { opacity: 0 } }
       animate={ { opacity: 1, transition: { duration: 0.2 } } }
     >
-      { props.children }
+      { children }
     </motion.div>
   );
 }
 
-function HorizontalTransitionAnimatedPage(props) {
+FadeTransitionAnimatedPage.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+function HorizontalTransitionAnimatedPage({ children }) {
   return (
     <motion.div
-      className="view-freak"
-      data-testid="view-freak"
       initial={ { y: '-100%' } }
       animate={ { y: 0, transition: { duration: 0.25 } } }
     >
-      { props.children }
+      { children }
     </motion.div>
   );
 }
 
-function VerticalTransitionAnimatedPage(props) {
+HorizontalTransitionAnimatedPage.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+function VerticalTransitionAnimatedPage({ children }) {
   return (
     <motion.div
-      className="view-freaks"
       initial={ { width: 0 } }
       animate={ { width: '100%', transition: { duration: 0.25 } } }
     >
-      { props.children }
+      { children }
     </motion.div>
   );
 }
+
+VerticalTransitionAnimatedPage.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export {
   HorizontalTransitionAnimatedPage,
