80f1053f95f86a52d8eb30eb00137caeb52d36fe
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-26 16:42:35 +0300

    Generalise AnimatedPage

--- src/components/AnimatedPages/AnimatedPages.jsx
+++ src/components/AnimatedPages/AnimatedPages.jsx
@@ -2,29 +2,37 @@ import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function FadeTransitionAnimatedPage({ children }) {
+function AnimatedPage({ initial, animate, children }) {
   return (
-    <motion.div
-      initial={ { opacity: 0 } }
-      animate={ { opacity: 1, transition: { duration: 0.2 } } }
-    >
+    <motion.div initial={ initial } animate={ animate }>
       { children }
     </motion.div>
   );
+}
+
+AnimatedPage.propTypes = {
+  initial: PropTypes.shape.isRequired,
+  animate: PropTypes.shape.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+function FadeTransitionAnimatedPage({ children }) {
+  return AnimatedPage({
+    initial: { opacity: 0 },
+    animate: { opacity: 1, transition: { duration: 0.2 } },
+    children,
+  });
 }
 
 FadeTransitionAnimatedPage.propTypes = {
   children: PropTypes.element.isRequired,
 };
 function HorizontalTransitionAnimatedPage({ children }) {
-  return (
-    <motion.div
-      initial={ { y: '-100%' } }
-      animate={ { y: 0, transition: { duration: 0.25 } } }
-    >
-      { children }
-    </motion.div>
-  );
+  return AnimatedPage({
+    initial: { y: '-100%' },
+    animate: { y: 0, transition: { duration: 0.25 } },
+    children,
+  });
 }
 
 HorizontalTransitionAnimatedPage.propTypes = {
@@ -32,14 +40,11 @@ HorizontalTransitionAnimatedPage.propTypes = {
 };
 
 function VerticalTransitionAnimatedPage({ children }) {
-  return (
-    <motion.div
-      initial={ { width: 0 } }
-      animate={ { width: '100%', transition: { duration: 0.25 } } }
-    >
-      { children }
-    </motion.div>
-  );
+  return AnimatedPage({
+    initial: { width: 0 },
+    animate: { width: '100%', transition: { duration: 0.25 } },
+    children,
+  });
 }
 
 VerticalTransitionAnimatedPage.propTypes = {
