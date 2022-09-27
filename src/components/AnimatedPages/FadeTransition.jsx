62f353502a776d612d7d910e04b81b430e5a7ba5
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-27 16:56:56 +0300

    Change FadeTransition from easeOut to easeIn

--- src/components/AnimatedPages/FadeTransition.jsx
+++ src/components/AnimatedPages/FadeTransition.jsx
@@ -4,7 +4,7 @@ import AnimatedPage from './AnimatedPages';
 function FadeTransition({ children }) {
   return AnimatedPage({
     initial: { opacity: 0 },
-    animate: { opacity: [ 0, 0, 0, 1 ], transition: { duration: 1, ease: 'easeOut' } },
+    animate: { opacity: [ 0, 0, 0, 1 ], transition: { duration: 1, ease: 'easeIn' } },
     exit: { opacity: 0, transition: { duration: 0.5, ease: 'easeIn' } },
     children,
   });
