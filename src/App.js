5e1b5cd8fba5c9fec6d7727bb7d4844f79c43cc4
Razvan Filip <filip.razvan@agilefreaks.com>
2022-10-10 10:38:25 +0300

    Add inputFilters to small screens filters menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faCaretDown, faTimes, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faCaretDown,
+  faTimes,
+  faUserPlus,
+} from '@fortawesome/free-solid-svg-icons';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import JellyText from './components/JellyText/JellyText';
 import { store } from './store/store';
--- src/components/OptionsBar/Options/FilterModal/FilterModalPage/FilterModalBody.jsx
+++ src/components/OptionsBar/Options/FilterModal/FilterModalPage/FilterModalBody.jsx
@@ -1,12 +1,23 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Modal } from 'react-bootstrap';
+import InputFilters from '../../../../InputFilters/InputFilters';
 
 function FilterModalBody({ filters, queuedFilters, onChange }) {
+  const [ inputPattern, setInputPattern ] = useState('');
+
+  const filterTest = (filter) => {
+    const filterName = filter.name.toLowerCase();
+    const filterPattern = inputPattern.toLowerCase();
+
+    return filterName.includes(filterPattern);
+  };
+
   return (
     <Modal.Body>
+      <InputFilters isOpen={ true } setFilteredText={ setInputPattern } />
       <Form>
-        { filters.map((filter) => (
+        { filters.filter(filterTest).map((filter) => (
           <div key={ filter.name } className="mb-3">
             <Form.Check
               type="checkbox"
